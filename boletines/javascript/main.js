4a372b8b25163d257e8c3faa11af9b8e2fcef97a
JorgeWGuzman <jwguzman@gmail.com>
2023-05-26 15:39:33 -0300

    230526 v12

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -1,5 +1,14 @@
+/*
 let links = document.querySelectorAll("a");
 
 links.forEach(function(link){
   console.log(link);
+});
+*/
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+  td.addEventListener('click',function(){
+    console.log("Click");
+  })
 });

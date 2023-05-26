48c450345ee949ac38735daf994e96354a1c270d
JorgeWGuzman <jwguzman@gmail.com>
2023-05-26 19:58:42 -0300

    230526 v35

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -1,22 +1,13 @@
-
-let links = document.querySelectorAll("a");
-
-links.forEach(function(link){
-  console.log(link);
-});
-
-
 let links = document.querySelectorAll(".close");
 
 links.forEach(function(link){
+  
   link.addEventListener("click",function(ev){
     ev.preventDefault();
     let content = document.querySelector('.content');
 
-
     content.classList.remove("animate__animated");
     content.classList.remove("animate__fadeInDown");
-
     content.classList.add("animate__animated");
     content.classList.add("animate__fadeOutUp");
 

60deda099b662cd8ec1a0f543bf328ead412cc9d
JorgeWGuzman <jwguzman@gmail.com>
2023-05-26 16:52:38 -0300

    230526 v21

--- boletines/javascript/main.js
+++ boletines/javascript/main.js
@@ -5,7 +5,7 @@ links.forEach(function(link){
   console.log(link);
 });
 */
-/*
+
 let links = document.querySelectorAll(".close");
 
 links.forEach(function(link){
@@ -13,15 +13,14 @@ links.forEach(function(link){
     ev.preventDefault();
     let content = document.querySelector('.content');
 
-    content.classList.remove("animate__fadeInDown");
     content.classList.remove("animate__animated");
-/*
-    content.classList.add("animate__fadeOutUp");
+    content.classList.remove("animate__fadeInDown");
+
     content.classList.add("animate__animated")
+    content.classList.add("animate__fadeOutUp");
 
     location.href = "/";
 
     return false;
   });
 });
-*/
